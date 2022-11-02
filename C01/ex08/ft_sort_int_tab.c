e6be5eed9dc7bea0773dfa69bdb3bb5a998ad525
Cédric V <101518014+cedvid@users.noreply.github.com>
2022-11-02 13:00:17 +0100

    Edit ft_sort_int_tab

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,11 +6,27 @@
 /*   By: cvidot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 11:20:07 by cvidot            #+#    #+#             */
-/*   Updated: 2022/10/20 09:07:30 by cvidot           ###   ########.fr       */
+/*   Updated: 2022/11/02 09:24:36 by cvidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp;
+			i = 0;
+		}
+		else
+			i++;
+	}
 }
