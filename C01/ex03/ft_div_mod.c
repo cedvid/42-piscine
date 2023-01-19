a3ea858fda420596bec617c0cdd54e822de34c44
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-19 14:31:44 +0100

    Add instruction

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+// Create a function ft_div_mod prototyped like this :
+// void ft_div_mod(int a, int b, int *div, int *mod);
+// This function divides parameters a by b and stores the result in the int pointed by
+// div. It also stores the remainder of the division of a by b in the int pointed by mod.
+// Allowed functions : None
+
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
