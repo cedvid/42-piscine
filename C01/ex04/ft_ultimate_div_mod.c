8c663c7896e427a83f9aaaa66729f1dc4980d2df
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-19 14:32:30 +0100

    Add instruction

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+// Create a function ft_ultimate_div_mod with the following prototype :
+// void ft_ultimate_div_mod(int *a, int *b);
+// This function divides parameters a by b. The result of this division is stored in the
+// int pointed by a. The remainder of the division is stored in the int pointed by b.
+// Allowed functions : None
+
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	div;
