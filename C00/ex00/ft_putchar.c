8a2e4801764d8d5c5e8ad5902a09e6bea7e29dfb
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-19 14:19:43 +0100

    add instruction

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -11,6 +11,9 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+//Write a function that displays the character passed as a parameter.
+//Allowed functions: write
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
