a9111fc8d8f3e4486dde3f99ea08ec7c6677abf2
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-19 14:29:22 +0100

    Add instruction

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -11,6 +11,11 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+// Create a function that takes a pointer to pointer to pointer to pointer to pointer
+// to pointer to pointer to pointer to pointer to int as a parameter and sets the value
+// "42" to that int.
+// Allowed functions : None
+
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
