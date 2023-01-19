f30ad1219e6d90c9ff5effb5face022f89951a4f
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-01-19 14:23:09 +0100

    Add instruction

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -11,6 +11,10 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+// Create a function that displays ’N’ or ’P’ depending on the integer’s sign entered
+// as a parameter. If n is negative, display ’N’. If n is positive or null, display ’P’.
+// Allowed functions: write
+
 void	ft_is_negative(int n)
 {
 	char	result;
